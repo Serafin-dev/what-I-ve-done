1c5c8049adbf38adea3a4cbbeb0fd40c4dfc78b8
Serafin-dev <82891246+Serafin-dev@users.noreply.github.com>
2022-05-17 15:15:37 -0300

    Update dna.py

--- Harvard-CS-Course/Serafin-dev-cs50-problems-2021-x-dna/dna.py
+++ Harvard-CS-Course/Serafin-dev-cs50-problems-2021-x-dna/dna.py
@@ -1,44 +1,57 @@
 import sys
 import csv
-#recursive function to count every matching sequence of every STR
-def rCount(st, iP, r):
-    if st == s[iP:iP + len(st)]:
-        r += 1
-        return rCount(st, iP + len(st), r)
-    return r
-#
-def compute(seq):
-    for k in data:
-        c = 0
-        for base in seq:
-            data[k].append(rCount(k, c, 0))
-            c += 1
-        data[k] = max(data[k])
 
-def compare():
-    with open(sys.argv[1]) as csvFile:
-        reader = csv.DictReader(csvFile)
-        for row in reader:
-            l = list(row.values())
-            l.remove(l[0])
-            if list(data.values()) == [int(i) for i in l]: 
-                match = row["name"]
-                return print(match)
+# recursive function to count every matching sequence of every STR
+def countStrRepetitions(dnaSequence, s, iPoint, counter):
+    if s == dnaSequence[iPoint: iPoint + len(s)]:
+        counter += 1
+        return countStrRepetitions(dnaSequence, s, iPoint + len(s), counter)
+    return counter
+
+# compare all str runs and select the maximum
+def computeLongestRun(dnaSequence):
+    for s in strs:
+        iPoint = 0
+        for base in dnaSequence:
+            strs[s].append(countStrRepetitions(dnaSequence, s, iPoint, 0))
+            iPoint += 1
+        strs[s] = max(strs[s])
+
+# Show the resulting match or not if there isn't
+def checkIfMatch(row, values):
+        if list(strs.values()) == [int(i) for i in values]:
+            match = row["name"]
+            return print(match)
         return print("No match")
-        
+
 if len(sys.argv) != 3:
     print("Usage: python dna.py data.csv sequence.txt")
     sys.exit(1)
 
-data = {}
-with open(sys.argv[1]) as csvFile:
-    strs = csvFile.readline().rstrip().split(",")
-    strs.remove("name")
-    for s in strs:
-        data[s] = []
-    
-with open(sys.argv[2], "r") as txtFile:
-    s = txtFile.read().rstrip()
-    l = []
-    compute(s)
-compare()
+def main():
+    with open(sys.argv[1], "r") as csvFile:
+        strsList = csvFile.readline().rstrip().split(",")
+        strsList.remove("name")
+        for s in strsList:
+            strs[s] = []
+
+        # compute longest run of each str for a given dna sequence
+        with open(sys.argv[2], "r") as txtFile:
+            dnaSequence = txtFile.read().rstrip()
+            computeLongestRun(dnaSequence)
+
+    # check match
+    with open(sys.argv[1], "r") as csvFile:
+        reader = csv.DictReader(csvFile)
+        for row in reader:
+            rowValues = list(row.values())
+            rowValues.remove(rowValues[0])
+
+            # Show the resulting match or not if there isn't
+            if list(strs.values()) == [int(i) for i in rowValues]:
+                match = row["name"]
+                return print(match)
+        return print("No match")
+
+strs = {}
+main()
